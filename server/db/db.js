49de56d75fa06dd571b9b97b1408e824682da5a5
Cervus18 <71794018+Cervus18@users.noreply.github.com>
2022-06-02 15:42:24 +0100

    Delete server directory

--- server/db/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const connectToDb = ()=>{
-    mongoose.connect('mongodb://localhost:27017/Learnia');
-    console.log("MongoDB connected !")
-}
-
-module.exports = connectToDb
